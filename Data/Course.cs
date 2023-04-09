a7e04940bb2a18bc418abcf57865856e5416594f
Evan_S <evansevy@evansevy.com>
2023-04-08 22:02:51 -0600

    Add base files

---
+++ Data/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace Asp_Ng_Sql_dot7_Test.Data
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ Data/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Asp_Ng_Sql_dot7_Test.Data
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
